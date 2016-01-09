67218acc0e4dfba243b06846596ca6f82747c14b
Vorobyov Kirill <vorobyovk@gmail.com>
2016-01-09 15:55:59 +0200

    Adding 6hw

---
+++ Chapter 6/hw-1/src/Flower.java
@@ -0,0 +1,12 @@
+public class Flower {
+    public String nameFlower;
+    public String colorFlower;
+
+    public void NewFlower(String nameFlower, String colorFlower ){
+        this.nameFlower = nameFlower;
+        this.colorFlower = colorFlower;
+
+
+    }
+
+}
---
+++ Chapter 6/hw-1/src/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+
+ public static void main(String[] args){
+  System.out.println("Please enter a name of you flower.");
+
+  System.out.println("Please enter a color of you flower.(Red, White, Blue, Yelow)");
+
+ }
+
+}
