121975d02cd1dd39dad8dbfd76af06b3f8b422f2
Vorobyov Kirill <vorobyovk@gmail.com>
2016-01-24 19:17:34 +0200

    edit file of chapter 8 home work

--- chapter 8/hw/src/MainClass.java
+++ chapter 8/hw/src/MainClass.java
@@ -1,39 +1,62 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
 
-    public static void main(String[] args){
-
-        System.out.println("Please select action.");
-        System.out.println("To adding a musical instrument - press 1.");
-        System.out.println("To print a array of musical instrument - press 2.");
-        System.out.println("To sorting of collection musical instrument - press 3.");
-        System.out.println("End program - press 4.");
-
+    public static void main(String[] args) throws IOException {
+        List<MusicalInstrument> instrumentList = new ArrayList<>();
         for(int i=0;i<4;i++){
-            final Scanner scaner = new Scanner(System.in);
-            int command = scaner.nextInt();
+            System.out.println("Please select action.");
+            System.out.println("To adding a musical instrument - press 1.");
+            System.out.println("To print a array of musical instrument - press 2.");
+            System.out.println("To sorting of collection musical instrument - press 3.");
+            System.out.println("End program - press 4.");
+
+            final Scanner scaner2 = new Scanner(System.in);
+            int command = scaner2.nextInt();
+            for ( int j = 0; j < 25; ++j )
+                System.out.println();
 
             if(command==1){
+              final Scanner scaner = new Scanner(System.in);
               System.out.println("Please enter a name of musical instrument.");
               String nameInstrument = scaner.nextLine();
               System.out.println("Please enter a color of musical instrument.");
               String colorInstrument = scaner.nextLine();
               System.out.println("Please enter a stringed or not of musical instrument.1/0");
-              Boolean strunniy = scaner.nextBoolean();
-              final List<MusicalInstrument> instrumentList = new ArrayList<>();
+              boolean strunniy = scaner.nextBoolean();
               instrumentList.add(new MusicalInstrument (nameInstrument,strunniy,colorInstrument));
+              for ( int j = 0; j < 25; ++j )
+                    System.out.println();
             } else if(command==2){
+                instrumentList.stream().forEach(instrument -> System.out.println(instrument));
+            }else {
+                if (command == 3) {
+                    int size = instrumentList.size();
+                    int checkStatus = size;
+                    System.out.println("Size of instrument array is: " + size);
+                    for (int j = 0; j < size; j++) {
+                        if (instrumentList.get(j).strunniy == false)
+                            if (instrumentList.get(checkStatus).strunniy == true) {
+                                String nameInstrument2 = instrumentList.get(checkStatus).nameInstrument;
+                                String colorInstrument2 = instrumentList.get(checkStatus).colorInstrument;
+                                boolean strunniy2 = instrumentList.get(checkStatus).strunniy;
+                                instrumentList.set(checkStatus, instrumentList.get(j).nameInstrument, instrumentList.get(j).strunniy, instrumentList.get(j).colorInstrument);
+                                instrumentList.set(j, nameInstrument2, strunniy2, colorInstrument2);
+                                checkStatus = checkStatus - 1;
+                                if (checkStatus == j) j = size;
+                            }
+                    }
 
-            }else if(command==3){
-
-            } else if(command==4){
-              break;
-            } else {
-              i=5;
-            };
+                } else if (command == 4) {
+                    break;
+                } else {
+                    break;
+                }
+            }
+            ;
             i--;
         };
 
--- chapter 8/hw/src/MusicalInstrument.java
+++ chapter 8/hw/src/MusicalInstrument.java
@@ -3,11 +3,18 @@ public class MusicalInstrument {
     public Boolean strunniy;
     public String colorInstrument;
 
-    public MusicalInstrument(final String nameInstrument, final Boolean strunniy, final String colorInstrument){
+    public MusicalInstrument(String nameInstrument, Boolean strunniy, String colorInstrument){
       this.nameInstrument  = nameInstrument;
       this.strunniy = strunniy;
       this.colorInstrument = colorInstrument;
     }
 
-
+    @Override
+    public String toString() {
+        return "MusicalInstrument{" +
+                "nameInstrument='" + nameInstrument + '\'' +
+                ", strunniy=" + strunniy +
+                ", colorInstrument='" + colorInstrument + '\'' +
+                '}';
+    }
 }
