0628352fc74cdb5398c4c65ae5f651bb5188d9c9
Vorobyov Kirill <vorobyovk@gmail.com>
2016-01-28 12:14:25 +0200

    adding chapter 3 files

---
+++ chapter 3/HomeWork3/ClassFlower.java
@@ -0,0 +1,35 @@
+public class Flower {
+  public String Name;
+  public String Color;
+  public Bool Smell;
+    public Vector  mybouquet of flowers;
+    public Vector  1;
+  public void consider() {
+  }
+  public void rip() {
+  }
+}
+
+public class Aster extends Flower {
+}
+
+public class Chamomile extends Flower {
+}
+
+public class Tulip extends Flower {
+}
+
+public class Rose extends Flower {
+   public Vector  myRosebush;
+   public Vector  1;
+}
+
+public class Rosebush {
+    public Vector  myRose;
+    public Vector  0..n;
+}
+
+public class bouquet of flowers {
+    public Vector  myFlower;
+    public Vector  1..n;
+}
