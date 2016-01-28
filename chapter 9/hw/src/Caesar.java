f7ace375c01219172ad20f122be9c03bc5584f5c
Vorobyov Kirill <vorobyovk@gmail.com>
2016-01-28 11:57:41 +0200

    editing files of chapter 9 home works

--- chapter 9/hw/src/Caesar.java
+++ chapter 9/hw/src/Caesar.java
@@ -1,68 +1,18 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
 public class Caesar {
-    private Reader in; // standard input stream for message
-    private int key; // (en|de)cryption key
+    public String s = "abcdefghijklmnopqrstuvwxyz";
+    public char c;
+    public int key;
+    public char encriptedChar;
 
-    // Caesar: constructor, opens standard input, passes key
-    public Caesar(int k) {
-        // open file
-        in = new InputStreamReader(System.in);
-        key = k;
-    }
-    // (en|de)crypt: just feed in opposite parameters
-    public void encrypt() { translate(key); }
-    public void decrypt() { translate(-key); }
-
-    // translate: input message, translate
-    private void translate(int k) {
-        char c;
-        while ((byte)(c = getNextChar()) != -1) {
-            if (Character.isLowerCase(c)) {
-                c = rotate(c, k);
-            }
-            System.out.print(c);
-        }
-    }
-
-    // getNextChar: fetches next char.
-    public char getNextChar() {
-        char ch = ' '; // = ' ' to keep compiler happy
-        try {
-            ch = (char)in.read();
-        } catch (IOException e) {
-            System.out.println("Exception reading character");
-        }
-        return ch;
-    }
-    // rotate: translate using rotation, version with table lookup
-    public char rotate(char c, int key) { // c must be lowercase
-        String s = "abcdefghijklmnopqrstuvwxyz";
+   public char Caesar(char c, int key){
+        this.c = c;
+        this.key=key;
         int i = 0;
         while (i < 26) {
             // extra +26 below because key might be negative
-            if (c == s.charAt(i)) return s.charAt((i + key + 26)%26);
+            if (c == s.charAt(i)) encriptedChar=s.charAt((i + key + 26)%26);
             i++;
         }
-        return c;
+        return encriptedChar;
     }
-
-
-    // main: check command, (en|de)crypt, feed in key value
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java Caesar (-d | -e) key");
-            System.exit(1);
-        }
-        Caesar cipher = new Caesar(Integer.parseInt(args[1]));
-        if (args[0].equals("-e")) cipher.encrypt();
-        else if (args[0].equals("-d")) cipher.decrypt();
-        else {
-            System.out.println("Usage: java Caesar (-d | -e) key");
-            System.exit(1);
-        }
-    }
-
 }
--- chapter 9/hw/src/MainClass.java
+++ chapter 9/hw/src/MainClass.java
@@ -1,9 +1,28 @@
 public class MainClass {
 
  public static void main(String[] args){
-     int i = 1;
-     Caesar caesar = new Caesar(i);
-     caesar.
+     String wordToEncript="world";
+     String encriptedWord="";
+     String decriptedWord="";
+     Caesar caesar = new Caesar();
 
+     int key = 1;
+
+     for(int i=0;i<wordToEncript.length();i++){
+         char c=wordToEncript.charAt(i);
+         caesar.Caesar(c,key);
+         c=caesar.encriptedChar;
+         encriptedWord=encriptedWord+c;
+     }
+
+    System.out.println("Encripted word = "+encriptedWord);
+
+     for(int i=0;i<encriptedWord.length();i++){
+         char c=encriptedWord.charAt(i);
+         caesar.Caesar(c,-key);
+         c=caesar.encriptedChar;
+         decriptedWord=decriptedWord+c;
+     }
+     System.out.println("Decripted word = "+decriptedWord);
  }
 }
